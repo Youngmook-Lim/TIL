21af19c3cd6e1f4705d8029b80279ab546bfb433
Youngmook Lim <youngmookk@gmail.com>
2022-12-09 22:05:57 +0900

    complete section 6

--- ReactJS_Udemy/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ ReactJS_Udemy/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -34,7 +34,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -78,6 +78,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button onClick={props.onCancel} type='button'>
+          Cancel
+        </button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
