0d9e9461fe21742257b483887513bd8efb22bbfd
Youngmook Lim <youngmookk@gmail.com>
2023-01-18 02:33:14 +0900

    Learn Inflearn JPA

--- Inflearn_JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,6 +14,32 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+
+            System.out.println("============");
+
+//            //비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//            //영속
+//            em.persist(member);
+//            em.detach(member);
+//            em.remove(member);
+
+//            Member findMember = em.find(Member.class, 101L);
+//            System.out.println(findMember.getId() + " " + findMember.getName());
+//            Member findMember2 = em.find(Member.class, 101L);
+//            System.out.println(findMember2 == findMember);
+
 //            Member member = new Member();
 //            member.setId(1L);
 //            member.setName("HelloA");
@@ -23,13 +48,13 @@ public class JpaMain {
 //            findMember.setName("HelloJPA");
 //            em.remove(findMember);
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
-                    .getResultList();
-            for (Member m : result) {
-                System.out.println(m.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(0)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//            for (Member m : result) {
+//                System.out.println(m.getName());
+//            }
 
             tx.commit();
         } catch (Exception e) {
