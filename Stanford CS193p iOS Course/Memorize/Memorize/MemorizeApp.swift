b6d285a9af1da4b31042d207d7c3dd259937ef86
Youngmook Lim <youngmookk@gmail.com>
2025-01-08 12:08:58 +0900

    Blank commit

---
+++ Stanford CS193p iOS Course/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Youngmook Lim on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
