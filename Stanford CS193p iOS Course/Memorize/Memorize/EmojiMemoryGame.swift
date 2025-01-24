0ed5fd4afdc0d52b08987523b6aa09fd019bc0df
Youngmook Lim <youngmookk@gmail.com>
2025-01-24 15:11:25 +0900

    Progress on card game

--- Stanford CS193p iOS Course/Memorize/Memorize/EmojiMemoryGame.swift
+++ Stanford CS193p iOS Course/Memorize/Memorize/EmojiMemoryGame.swift
@@ -7,19 +7,35 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    static let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙‍♀️", "🙀", "👹", "😱", "☠️", "🍭"]
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙‍♀️", "🙀", "👹", "😱", "☠️", "🍭"]
     
-    private var model = MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
-        return emojis[pairIndex]
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 16) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+            
+        }
     }
     
+    @Published private var model = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
     }
     
     func choose(_ card: MemoryGame<String>.Card) {
         model.choose(card)
     }
 }
+
+   
